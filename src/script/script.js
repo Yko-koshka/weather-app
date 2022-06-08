957d17f6466e4bbfdea7c6d79c28dc561e3c48ed
Yko-koshka <legezaanna7@gmail.com>
2022-06-08 18:54:20 +0200

    added icon

--- src/script/script.js
+++ src/script/script.js
@@ -14,20 +14,24 @@ function showData() {
     hour: "2-digit",
     minute: "2-digit",
   })}`;
-  console.log(data.getDay());
 }
 showData();
 
 let apiKey = "b008b611bf4075eb12ea48ff1a84b599";
 
 function showDegree(response) {
-  console.log(response);
-  let showDegree = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+  let showDegree = Math.round(celsiusTemperature);
   let showWind = Math.round(response.data.wind.speed);
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
   let getDegree = document.querySelector("#show-degree");
+  let showIcon = document.querySelector("#icon");
 
+  showIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   getDegree.innerHTML = `${showDegree}`;
   humidity.innerHTML = `${response.data.main.humidity}`;
   wind.innerHTML = `${showWind}`;
@@ -50,7 +54,10 @@ submit.addEventListener("submit", showCity);
 function snowFahrenheit(event) {
   event.preventDefault();
   let degree = document.querySelector("#show-degree");
-  degree.innerHTML = `${Math.round((degree.innerHTML * 9) / 5 + 32)}`;
+  degree.innerHTML = `${Math.round((celsiusTemperature * 9) / 5 + 32)}`;
+
+  celsius.classList.remove("active");
+  fahrenheit.classList.add("active");
 }
 let fahrenheit = document.querySelector("#show-fahrenheit");
 fahrenheit.addEventListener("click", snowFahrenheit);
@@ -58,41 +65,13 @@ fahrenheit.addEventListener("click", snowFahrenheit);
 function showCelsius(event) {
   event.preventDefault();
   let degree = document.querySelector("#show-degree");
-  degree.innerHTML = "15";
+  degree.innerHTML = Math.round(celsiusTemperature);
+
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
 }
+
+let celsiusTemperature = null;
+
 let celsius = document.querySelector("#show-celsius");
 celsius.addEventListener("click", showCelsius);
-
-// Geolocation
-
-function showGeolocation(response) {
-  let showDegreeGeolocation = Math.round(response.data.main.temp);
-  let showCityGeolocation = response.data.name;
-  let showCity = document.querySelector("#show-city");
-  let getWindRound = Math.round(response.data.wind.speed);
-  let getHumidity = document.querySelector("#humidity");
-  let getWind = document.querySelector("#wind");
-  let showDegree = document.querySelector("#show-degree");
-
-  showCity.innerHTML = `${showCityGeolocation}`;
-  showDegree.innerHTML = `${showDegreeGeolocation}`;
-  getHumidity.innerHTML = `${response.data.main.humidity}`;
-  getWind.innerHTML = `${getWindRound}`;
-}
-
-let getLocation = document.querySelector("#get-location");
-getLocation.addEventListener("click", (event) => {
-  event.preventDefault();
-  if ("geolocation in naviganor") {
-    navigator.geolocation.getCurrentPosition((position) => {
-      let lat = position.coords.latitude;
-      let lon = position.coords.longitude;
-      let positionCity = `lat=${lat}&lon=${lon}`;
-      let apiUrlPosition = `https://api.openweathermap.org/data/2.5/weather?${positionCity}&units=metric&appid=${apiKey}`;
-
-      axios.get(`${apiUrlPosition}`).then(showGeolocation);
-    });
-  } else {
-    console.log("error");
-  }
-});
